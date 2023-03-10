e4e7ff497e54ed0cb130a5e05de4abd8a796ff97
Administrator <3098016342@qq.com>
2023-03-10 13:25:07 +0800

    第一次尝试KNN

--- homework2/1.py
+++ homework2/1.py
@@ -10,11 +10,17 @@ df_answer = pd.read_csv('标准答案.csv')
 df_test = np.array(df_test)
 df_train = np.array(df_train)
 df_answer = np.array(df_answer)
-answer = []
 df_answer = np.array(df_answer)
-for i in range(28000):
-    answer.append(df_answer[i][1])
-print(answer)
+def caculate_distance(test, train):
+    dist = 0
+    a = 0
+    for i in range(783):
+        s = (float(test[i]) - float(train[i + 1])) ** 2
+        #   print(s)
+        a += 1
+        dist += s
+    distance_ = (dist/783) ** 0.5
+    print(distance_)
+    return distance_
 
-print(df_train)
-print(df_train[1][0])
+caculate_distance(df_test[0], df_train[0])
